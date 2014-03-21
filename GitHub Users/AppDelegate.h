d4e2fc422d6eff404ca709543b849b8c329488c8
Artem Podustov <a.podustov@gmail.com>
2014-03-21 17:21:58 +0200

    Initial Commit

---
+++ GitHub Users/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitHub Users
+//
+//  Created by Artem Podustov on 3/21/14.
+//  Copyright (c) 2014 OLEArt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHub UsersTests/GitHub_UsersTests.m
@@ -0,0 +1,34 @@
+//
+//  GitHub_UsersTests.m
+//  GitHub UsersTests
+//
+//  Created by Artem Podustov on 3/21/14.
+//  Copyright (c) 2014 OLEArt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GitHub_UsersTests : XCTestCase
+
+@end
+
+@implementation GitHub_UsersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
